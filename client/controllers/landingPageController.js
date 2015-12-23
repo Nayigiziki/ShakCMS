424b01dfbdc33938ee107b4a88a71beab1905672
Nayigiziki <josephnayigiziki@gmail.com>
2015-12-22 18:15:25 -0800

    fixed safari alignment in main.css

--- client/controllers/landingPageController.js
+++ client/controllers/landingPageController.js
@@ -12,6 +12,6 @@ angular.module('shakApp.landingPage', [])
       })
     }
 
-    Materialize.toast('Welcome to shakeil.com', 1000000);
+    Materialize.toast('Welcome to shakeil.com', 4000);
 
   });
